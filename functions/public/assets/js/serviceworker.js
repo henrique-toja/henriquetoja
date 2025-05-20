bd437c4cc62348b3bd3f8fbcc4dbe183a3e44250
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2025-05-20 20:52:39 -0300

    Atualizar o serviceworker.js

--- functions/public/assets/js/serviceworker.js
+++ functions/public/assets/js/serviceworker.js
@@ -2,9 +2,16 @@ const CACHE_NAME = "toja-pwa-v1";
 const urlsToCache = [
   "/",
   "/index.html",
+  "/manifest.json",
+  "/assets/js/serviceworker.js",
+  "/assets/js/header.js",
+  "/assets/js/footer.js",
+  "/assets/js/chat.js",
+  "/assets/js/body.js",
+  "/assets/js/database.js",
   "/assets/images/logo.webp",
-  "/assets/images/logo.webp",
-  // adicione outros assets críticos (css, js, fonts)
+  "/assets/images/henriquetoja.jpg",
+  "/assets/css/style.css"
 ];
 
 self.addEventListener("install", event => {
