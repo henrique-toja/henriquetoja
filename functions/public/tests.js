d1e98c27380cc41c846ddd7011e97d87a82d8df2
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2025-06-15 06:31:49 -0300

    Atualizar o tests.js

--- functions/public/tests.js
+++ functions/public/tests.js
@@ -20,7 +20,7 @@ function startTest() {
                     Teste de Personalidade
                 </button>
                 <button id="btn-debate" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-lg transition-colors text-lg shadow">
-                    Carregar Debate
+                    Debate IAs
                 </button>
             </div>
         </div>
